591a0a222a07bf560010366e1b60725ad73e8a0d
Juliors1 <user@email.com>
2020-12-01 22:00:50 -0500

    set up model and controller folder

---
+++ dev-express-lab/routes/skills.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+const Skill = require("../../models/skill");
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.render("/skills/index", {
+    skills: Skill.getAllSkills(),
+  });
+});
+
+module.exports = router;
---
+++ models/skill.js
@@ -0,0 +1,14 @@
+const skills = [
+  {id:123, skill:'html', done: true},
+  {id:1234, skill:'css', done: true},
+  {id:12345, skill:'javascript', done: true},
+  {id:123456, skill:'node', done: false}
+]
+
+module.exports = {
+  getAllSkills
+
+}
+function getAllSkills() {
+  return skills;
+}
